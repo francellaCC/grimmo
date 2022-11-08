397783b95c75a3e6058da7a5376c2f2ea4cbfbb1
ESTEBAN ARIAS <estebanarias@ESTEBANs-MacBook-Pro-2.local>
2022-11-08 00:16:42 -0600

    Diseno Jugador y habilidades (Jump, Walk, Shoot, Respawn) v1, Mapa 1 funcional

--- Grimmo/Assets/Scripts/Player.cs
+++ Grimmo/Assets/Scripts/Player.cs
@@ -4,28 +4,37 @@ using System.Collections.Generic;
 // using Spine.Unity;
 using UnityEngine;
 using static UnityEngine.Physics;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     private Rigidbody2D Rigidbody2D;   
     private bool Grounded;
-    public float walkSpeed = 3f;
-
-    //rebote
-    public Vector2 velocidadRebote;
+    public float walkSpeed = 4f;
     public float JumpForce;
+    public Vector2 velocidadRebote;
+
+    public int life = 2;
+    public GameObject bulletPrefab;
+    private float lastShoot;
+    public Vector2 respawnpoint;
+    private Vector2 initialPosition;
+    private bool isInCooldown;
+    public float cooldownTime = 1f;
+
     // private SkeletonAnimation skeletonAnimation;
-    // private string previousState, currentState;
-    // public GameObject[] waterSurface;
-    // private bool isFloating = false;
+    private string previousState, currentState;
     private float xAxis;
     private float yAxis;
+    private bool climbingAllowed = false;
 
     // Start is called before the first frame update
     void Start()
     {
         // skeletonAnimation = GetComponent<SkeletonAnimation>();
         Rigidbody2D = GetComponent<Rigidbody2D>();
+        initialPosition = transform.position;
+        respawnpoint = initialPosition;
 
     }
 
@@ -37,11 +46,11 @@ public class Player : MonoBehaviour
 
          if(xAxis < 0.0f) //Si va a la izquierda
         {
-            transform.localScale = new Vector3(-0.4f, 0.477415f, 0.477415f); //Se pone el eje "y" en negativo y se gira el personaje
+            transform.localScale = new Vector3(-0.15f, 0.14f, 0.15f); //Se pone el eje "y" en negativo y se gira el personaje
         }
         else if(xAxis > 0.0f) //Si va a la derecha
         {
-            transform.localScale = new Vector3(0.4f, 0.477415f, 0.477415f);
+            transform.localScale = new Vector3(0.15f, 0.14f, 0.15f);
         }
 
         // currentState = xAxis == 0 ? "Idle" : "Run"; //Si el hAxis es 0, el estado es Idle, si no, es Run
@@ -59,9 +68,13 @@ public class Player : MonoBehaviour
             Grounded = false;
             }
 
-        if((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow)) && Grounded)
+        if((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Space)) && Grounded)
         {
             Rigidbody2D.AddForce(Vector2.up * JumpForce); //El .up signofica que el eje "x=0" y "y=1"
+        }
+
+        if(climbingAllowed){
+            yAxis *= walkSpeed;
         }
         
         //-----Eje Y------
@@ -75,23 +88,132 @@ public class Player : MonoBehaviour
 
         // previousState = currentState;
         // Cambio de animaciones
-        
-        // if(isFloating){
-        //     walkSpeed = 0.5f;
+
+        if (Input.GetKeyDown(KeyCode.E) && Time.time > lastShoot + 1f)
+        {
+            Shoot();
+            lastShoot = Time.time;
+        }
+
+        // if(Input.GetKeyDown(KeyCode.S) && destinyWarp){
+        //     transform.position = destinyWarp.transform.position;
         // }
 
+        DeathOnFall();
+
     }
+        
 
     private void FixedUpdate()  //FixedUpdate se usa siempre que trabajemos con fisicas ya que se tienen que actualizar con mucha frecuencia
     {
         //velocity espera un vector2 = dos elementos indican la "x" y "y" del mundo
         Rigidbody2D.velocity = new Vector2(xAxis * walkSpeed, Rigidbody2D.velocity.y);
+
+        if(climbingAllowed){
+            Rigidbody2D.isKinematic = true;
+            Rigidbody2D.velocity = new Vector2(xAxis, yAxis);
+        }else{
+            Rigidbody2D.isKinematic = false;
+            Rigidbody2D.velocity = new Vector2(xAxis * walkSpeed, Rigidbody2D.velocity.y);
+        }
+
+        // if(!isInCooldown){
+        // Rigidbody2D.velocity = new Vector2(xAxis, Rigidbody2D.velocity.y);
+        // }
     }
 
-//cuando el jugador es tocado por el enemigo, el jugador
-//rebota a la dirrecion contraria
-    public void Rebote(Vector2 puntoGolpe){
-       Rigidbody2D.velocity= new Vector2(-velocidadRebote.x* puntoGolpe.x,velocidadRebote.y);
+    private void OnTriggerEnter2D(Collider2D collider) 
+    {
+        if(collider.CompareTag("Ladder")){
+            climbingAllowed = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider) 
+    {
+        //Al salir de cualquier collider
+        walkSpeed = 4f;
+
+        if(collider.CompareTag("Ladder")){
+            climbingAllowed = false;
+        }
+    }
+
+    //cuando el jugador es tocado por el enemigo, el jugador
+    //rebota a la dirrecion contraria
+    public void Rebote(Vector2 puntoGolpe)
+    {
+       Rigidbody2D.velocity= new Vector2(-velocidadRebote.x * puntoGolpe.x,velocidadRebote.y);
+    }
+
+    public void Death()
+    {
+        transform.position = initialPosition;
+        respawnpoint = initialPosition;
+        if (life <= 0)
+        {
+            life = 2;
+            //Todavia no tenemos el panel
+            // for (int i = 0; i < lifesPanel.transform.childCount; i++)
+            // {
+            //     lifesPanel.transform.GetChild(i).gameObject.SetActive(true);
+            // }
+        }
+    }
+
+    public void Hit(float knockback, GameObject enemy)
+    {
+        if(!isInCooldown){
+        StartCoroutine(cooldown());
+        if (life > 0)
+        {
+            //lifesPanel.transform.GetChild(life).gameObject.SetActive(false);
+            life -= 1;
+            if(enemy)
+            {
+                Vector2 difference = (transform.position - enemy.transform.position);
+                float knockbackDirection = difference.x >= 0? 1 : -1; //Esto es como un if. Si es > - me tira 1 o sino -1
+                Rigidbody2D.velocity = new Vector2(knockbackDirection * knockback, knockback/2);
+            }
+
+        }
+        else
+        {
+            Death();
+        }
+        }
+    }
+
+    IEnumerator cooldown()
+    {
+        isInCooldown = true;
+        yield return new WaitForSeconds(cooldownTime);
+        isInCooldown = false;
+    }
+
+    private void DeathOnFall()
+    {
+        if (transform.position.y < -10f)
+        {
+            transform.position = respawnpoint;
+            Hit(0, null);
+        }
+    }
+
+    public void Shoot()
+    {
+        Vector3 direction;
+        if (transform.localScale.x > 0)
+            direction = Vector3.right;
+        else
+            direction = Vector3.left;
+        GameObject bullet =
+            Instantiate(bulletPrefab,
+            transform.position + direction * 0.8f,
+            Quaternion.identity);
+        bullet.GetComponent<Bullet>().SetDirection(direction);
     }
 
 }
+
+
